e95b1951574e6363c9b5f5410c5f9ca34d938bb7
ShariqAteeq <m.shariq22@hotmail.com>
2022-11-29 01:36:56 +0500

    worked on static images

--- src/api/resolver/test.resolver.ts
+++ src/api/resolver/test.resolver.ts
@@ -7,8 +7,16 @@ import {
   Resolver,
 } from '@nestjs/graphql';
 import { InjectRepository } from '@nestjs/typeorm';
+import { createWriteStream } from 'fs';
 import { Repository } from 'typeorm';
 import { Test } from '../entities/test';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
+import { ulid } from 'ulid';
+// @ts-ignore
+// import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
+// @ts-ignore
+// import FileUpload from 'graphql-upload/Upload';
+// const GraphQLUpload = require('graphql-upload/GraphQLUpload.mjs');
 
 @Resolver(() => Test)
 export class TestResolver {
@@ -29,12 +37,27 @@ export class TestResolver {
   async typeDetail(@Parent() test: Test) {
     const msg = {
       __typename: 'Message',
-      message: 'Hi from Message resrolver',
+      message: 'Hi from Message resrolvers ',
     };
     const convo = {
       __typename: 'Conversation',
       lastMessage: 'HI from Convo resrolver',
     };
     return [msg, convo];
+  }
+
+  @Mutation(() => Boolean)
+  async uploadFile(
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    { createReadStream, filename }: FileUpload,
+  ): Promise<boolean> {
+    const fname = `${ulid()}_${filename}`;
+    console.log('fname', fname);
+    return new Promise(async (resolve, reject) =>
+      createReadStream()
+        .pipe(createWriteStream(`./assets/${fname}`))
+        .on('finish', () => resolve(true))
+        .on('error', () => reject(false)),
+    );
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { BootstrapModule } from './setup/bootstrap.module';
 
 @Module({
-  imports: [BootstrapModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
+      serveStaticOptions: {
+        redirect: false,
+        index: false,
+      },
+    }),
+    BootstrapModule,
+  ],
   controllers: [],
   providers: [],
 })
